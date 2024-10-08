9d973b9d5d143df9b87d17e9955400b31730299a
Yevhen <yevhen.kulesgir@gmail.com>
2024-10-08 16:10:23 +0200

    Перший коміт: додано проєкт

---
+++ Lab2_Kuleshir/Lab2_Kuleshir/Lab2_Kuleshir/Controllers/ResolveController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourAppNamespace.Controllers
+{
+    public class ResolveController : Controller
+    {
+        
+        public IActionResult Show()
+        {
+            ViewData["Message"] = "Це нове представлення, створене в ResolveController.";
+            return View();
+        }
+    }
+}
